ed4690845835ed5bcb0a1e332a1e4faae03ff19f
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-03-30 16:34:31 -0500

    Municipio Delete

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -101,6 +101,14 @@ class MunicipioController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $municipio = Municipio::find($id);
+        $municipio->delete();
+
+         $municipios = DB::table('tb_municipio')
+         ->join('tb_departamento','tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+         ->select('tb_municipio.*','tb_departamento.depa_nomb')
+        ->get();
+
+        return view('municipio.index', ['municipios' => $municipios]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('c
 Route::get('/municipios',  [MunicipioController::class, 'index'])->name('municipios.index');
 Route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
 Route::get('/municipios/create', [MunicipioController::class, 'create'])->name('municipios.create');
+Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
