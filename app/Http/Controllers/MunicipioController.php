804c6155bb91998ae6fc7223a14ad2b68e540b97
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-03-30 16:20:18 -0500

    Ajustes de la vista de listado de Municipios

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Municipio;
+use Illuminate\Support\Facades\DB;
 
 class MunicipioController extends Controller
 {
@@ -14,7 +15,11 @@ class MunicipioController extends Controller
      */
     public function index()
     {
-        $municipios = Municipio::all();
+        //$municipios = Municipio::all();
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento','tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+        ->get();
         return view('municipio.index', ['municipios' => $municipios]);
 
     }
