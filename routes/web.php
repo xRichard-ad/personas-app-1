4579e531e003af84f808e4eeb9b75bbfae4a1e56
Richard Alvarez <richardalvarez1207@gmail.com>
2024-04-14 15:41:38 -0500

    Bfeeze dev

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\ComunaController;
-use App\Http\Controllers\DepartamentoController;
-use App\Http\Controllers\MunicipioController;
-use App\Http\Controllers\PaisController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -21,36 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/comunas',  [ComunaController::class, 'index'])->name('comunas.index');
-Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-Route::put('/comunas/{comuna}', [ComunaController::class, 'update']) ->name('comuna.update');
-Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Rutas de Municipio
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/municipios',  [MunicipioController::class, 'index'])->name('municipios.index');
-Route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
-Route::get('/municipios/create', [MunicipioController::class, 'create'])->name('municipios.create');
-Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
-Route::put('/municipios/{municipio}', [MunicipioController::class, 'update']) ->name('municipios.update');
-Route::get('/municipios/{municipio}/edit', [MunicipioController::class, 'edit'])->name('municipios.edit');
-
-//Rutas de Departamento
-
-Route::get('/departamentos',  [DepartamentoController::class, 'index'])->name('departamentos.index');
-Route::post('/departamentos', [DepartamentoController::class, 'store'])->name('departamentos.store');
-Route::get('/departamentos/create', [DepartamentoController::class, 'create'])->name('departamentos.create');
-Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
-Route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update']) ->name('departamentos.update');
-Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
-
-//Rutas de Pais
-
-Route::get('/paises',  [PaisController::class, 'index'])->name('paises.index');
-Route::post('/paises', [PaisController::class, 'store'])->name('paises.store');
-Route::get('/paises/create', [PaisController::class, 'create'])->name('paises.create');
-Route::delete('/paises/{pais}',[PaisController::class,'destroy'])->name('paises.destroy');
-Route::put('/paises/{pais}', [PaisController::class, 'update']) ->name('paises.update');
-Route::get('/paises/{pais}/edit', [PaisController::class, 'edit'])->name('paises.edit');
+require __DIR__.'/auth.php';
